5c7e2106401d120bdea5bf161045fb57036d60bb
vachan-maker <vachan@tutanota.com>
2025-07-14 19:44:58 +0530

    Started hero page

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,4 +1,10 @@
 function Hero() {
-    rturn ()
+    return (
+        <>
+        <section className="h-dvh bg-[radial-gradient(circle,_#1e4620_0%,_#143017_60%,_#000000_100%)] flex flex-col">
+            <h1 className="text-center">നല്ല ഫ്രഷ് ചക്ക!</h1>
+        </section>
+        </>
+    )
 }
 export default Hero
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero"
 function Home() {
   return (
     <>
