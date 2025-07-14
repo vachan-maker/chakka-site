c9acc3f90699889b053c12290b7913420dd260a5
vachan-maker <vachan@tutanota.com>
2025-07-14 20:47:12 +0530

    Replaced title and head

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 const RIT_Kutty = localFont({
   src: '../../../fonts/RIT-Kutty-Bold.woff2',
   display: 'swap',
-  variable: '--font-RIT-Kutty',
 });
 function Hero() {
 
@@ -13,7 +12,7 @@ function Hero() {
         <>
         <section className="h-dvh bg-[radial-gradient(circle,_#1e4620_0%,_#143017_60%,_#000000_100%)] flex flex-col items-center justify-center mx-auto">
             <Image src={chakka} width={1024} height={1024} alt='Image of a jackfruit' className='w-75 md:w-90 mx-auto'/>
-            <h1 className={`${RIT_Kutty.className} text-center text-[#fcce0c] text-8xl p-5 md:text-9xl`}>നല്ല ഫ്രഷ് ചക്ക!</h1>
+            <h1 className={`${RIT_Kutty.className} text-center text-[#fcce0c] text-8xl font-bold p-5 md:text-9xl`}>നല്ല ഫ്രഷ് ചക്ക!</h1>
         </section>
         </>
     )
