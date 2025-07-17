a549529f5a764f6e796b1e587707ba1437df270c
vachan-maker <vachan@tutanota.com>
2025-07-17 19:02:30 +0530

    Added confetti

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,4 +1,5 @@
 'use client'
+import confetti from 'canvas-confetti';
 import { useRouter } from 'next/navigation';
 import localFont from 'next/font/local';
 import chakka from '../../../public/chakka3.png'
@@ -9,13 +10,44 @@ const RIT_Kutty = localFont({
 });
 function Hero() {
     const router = useRouter()
+    const handleClick = () => {
+    const duration = 5 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+ 
+    const randomInRange = (min, max) =>
+      Math.random() * (max - min) + min;
+ 
+    const interval = window.setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+ 
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+ 
+      const particleCount = 50 * (timeLeft / duration);
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      });
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      });
+    }, 250);
+  };
 
     return (
         <>
         <section className="h-dvh flex flex-col items-center justify-center mx-auto">
             <Image src={chakka} width={1024} height={1024} alt='Image of a jackfruit' className='w-75 md:w-90 mx-auto'/>
             <h1 className={`${RIT_Kutty.className} text-center text-[#fcce0c] text-8xl font-bold p-5 md:text-9xl`}>നല്ല ഫ്രഷ് ചക്ക!</h1>
+            <div className='flex flex-row gap-5'>
             <button className='btn btn-outline border-[#fcce0c] btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl' onClick={() => router.push('/explore')}>Explore &rarr;</button>
+            <button className='btn btn-outline border-[#fcce0c] btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl' onClick={() => handleClick()}>Adi Pookutty!!</button>
+            </div>
         </section>
         </>
     )
