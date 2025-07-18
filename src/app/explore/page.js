eb2d241f3b9e04bb5b6848bb715d750b3fb099fb
vachan-maker <vachan@tutanota.com>
2025-07-18 08:04:22 +0530

    Use a dict for storing all the projects and map through it...code is much more cleaner now!

--- src/app/explore/page.js
+++ src/app/explore/page.js
@@ -3,43 +3,63 @@ import localFont from 'next/font/local';
 import Image from 'next/image'
 import Link from 'next/link';
 const RIT_Kutty = localFont({
-  src: '../../../fonts/RIT-Kutty-Bold.woff2',
-  display: 'swap',
+    src: '../../../fonts/RIT-Kutty-Bold.woff2',
+    display: 'swap',
 });
+const services = [
+    {
+        id:1,
+        name:"QR Code Generator",
+        description:"Convert text/URL to QR Codes!",
+        selfurl:"/qrcode"
+    },
+    {
+        id:2,
+        name:"Roll a dice!",
+        description: "what the name says",
+        selfurl:"/dice"
+    },
+    {
+        id:3,
+        name:"Nira",
+        description: "A puzzle game",
+        exturl:"https://nira20.vercel.app"
+    },
+    {
+        id:4,
+        name:"Space Hub",
+        description: "Reach for the cosmos!",
+        exturl:"https://vachan-maker.github.io/space-hub/index.html"
+    },
+    {
+        id:5,
+        name:"Business Card Creator",
+        description:"Create a simple business card for yourself",
+        exturl: "http://vachan-maker.github.io/business-card-creator/"
+    }
+]
 function Services() {
-    return(
+    return (
         <>
-        <div className="flex flex-row min-h-dvh">
-            <div className="flex flex-col items-center justify-center  border-amber-300 border-r-2">
-                <Link href='/'>
-                <Image src={chakka} width={1024} height={1024} alt='Image of a jackfruit' className='w-75 md:w-75 mx-auto p-10'/>
-                </Link>
-            </div>
-            <div className='min-h-dvh p-10'>
-                <Link href="/qrcode">
-                <div className='border border-amber-200 rounded-md p-10 hover:bg-[#040a05] cursor-pointer'>
-                    <p>QR Code Generator</p>
+            <div className="flex flex-col md:flex-row min-h-dvh">
+                <div className="flex flex-col items-center justify-center  border-amber-300 border-r-2">
+                    <Link href='/'>
+                        <Image src={chakka} width={1024} height={1024} alt='Image of a jackfruit' className='w-75 md:w-75 mx-auto p-10' />
+                    </Link>
                 </div>
-                </Link>
-                <Link href="/dice">
-                <div className='border border-amber-200 rounded-md p-10 hover:bg-[#040a05] cursor-pointer'>
-                    <p>Roll a Dice</p>
+                <div className='min-h-dvh p-10'>
+                    {services.map((service)=>{
+                        return (<Link href={service.selfurl?service.selfurl:service.exturl} key={service.id}>
+                            <div className='border border-amber-200 rounded-md p-5 hover:bg-[#040a05] cursor-pointer m-5'>
+                                <p>{service.name}</p>
+                                <p>{service.description}</p>
+                            </div>
+                        </Link>);
+                    })}
                 </div>
-                </Link>
-
             </div>
-                        <div className='min-h-dvh p-10'>
-                <Link href="https://nira20.vercel.app">
-                <div className='border border-amber-200 rounded-md p-10 hover:bg-[#040a05] cursor-pointer'>
-                    <p>Nira</p>
-                    <p>Developed by Abhiram A.K</p>
-                </div>
-                </Link>
-
-            </div>
-        </div>
         </>
-      
+
     )
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[radial-gradient(circle,_#1e4620_0%,_#143017_60%,_#000000_100%)] min-h-dvh">
+      <body className="min-h-dvh">
         {children}
       </body>
     </html>
