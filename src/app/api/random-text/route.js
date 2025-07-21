6568019b723fed584727f485ea1eeebc2b681f03
vachan-maker <vachan@tutanota.com>
2025-07-21 16:48:45 +0530

    fix bug in api

--- src/app/api/random-text/route.js
+++ src/app/api/random-text/route.js
@@ -11,5 +11,5 @@ export async function GET(request) {
     model: "gemini-2.5-flash",
     contents: "Generate a long, coherent, and meaningful paragraph in Malayalam (around 150–200 words). The content should be general, safe for all audiences, and free from political, religious, or controversial themes. It should reflect positive or neutral ideas, use standard grammar and vocabulary, and be suitable for general reading by people of all ages. Avoid any offensive or culturally inappropriate references.Do not include any explanations, introductions, or English text—only output the paragraph.",
   });
-    return new NextResponse.json({ response: result.text });
+    return NextResponse.json({ response: result.text });
 }
