1a6c415532e9242656d311e74a7f7066b019a847
vachan-maker <vachan@tutanota.com>
2025-07-26 23:52:32 +0530

    Use suspense for searchparams as without it netlify cannot build the app

--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,14 +1,16 @@
 'use client'
 import {useSearchParams} from "next/navigation";
-
+import {Suspense} from "react";
 function Result() {
     const text = useSearchParams().get('text');
     const decodedText = decodeURI(text);
     return(
+        <Suspense>
         <div className='bg-[#f4ecd8] text-[#5b4636] min-h-dvh font-serif text-xl p-6'>
             <h1 className="text-4xl text-center font-bold mb-10">One Message. Infinite Strength.</h1>
         <p className='max-w-[65ch] my-0 mx-auto'>{decodedText}</p>
         </div>
+        </Suspense>
     )
 }
 
