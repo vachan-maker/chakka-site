fc58ad43db1822e5ffef9c501f3e14c0cc268f68
vachan-maker <vachan@tutanota.com>
2025-07-27 08:01:48 +0530

    fixed result page for motivation by breaking it down in to components and using props

--- src/app/components/Result.jsx
+++ src/app/components/Result.jsx
@@ -1,8 +1,8 @@
-function Result(props) {
+function ResultComponent({text}) {
     return (        <div className='bg-[#f4ecd8] text-[#5b4636] min-h-dvh font-serif text-xl p-6'>
         <h1 className="text-4xl text-center font-bold mb-10">One Message. Infinite Strength.</h1>
-        <p className='max-w-[65ch] my-0 mx-auto'>{props.decodedText}</p>
+        <p className='max-w-[65ch] my-0 mx-auto'>{text}</p>
     </div>);
 }
 
-export default Result;
+export default ResultComponent;
--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,11 +1,14 @@
 'use client'
 import {useSearchParams} from "next/navigation";
+import ResultComponent from "../components/Result"
 import {Suspense} from "react";
 function Result() {
     const text = useSearchParams().get('text');
     const decodedText = decodeURI(text);
+    console.log(decodedText);
     return(
         <Suspense>
+            <ResultComponent text={decodedText}/>
         </Suspense>
     )
 }
