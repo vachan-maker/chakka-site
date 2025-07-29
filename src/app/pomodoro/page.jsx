c725b241cf7dcfbf34be2795933832625f9e1604
vachan-maker <vachan@tutanota.com>
2025-07-29 19:25:05 +0530

    conditional rendering

--- src/app/pomodoro/page.jsx
+++ src/app/pomodoro/page.jsx
@@ -2,21 +2,24 @@
 import Image from "next/image";
 import {useState,useRef} from "react";
 function PomodoroTimer() {
-    const [time,setTime] = useState(25*60);
+    const [time,setTime] = useState((1/5)*60);
     const [start,setStart] = useState(false);
     const intervalRef = useRef(null);
+    const [rest,setRest] = useState(false);
 
     const startTimer = () => {
         setStart(true);
-        if(start){return;}
+        if(intervalRef.current != null){return;}
         intervalRef.current = setInterval(()=>{
-            if(time>0) {
-                setTime(prev => prev-1);
-            }
-
-            if(time === 0) {
-                clearInterval(intervalRef.current);
-            }
+            setTime(prev => {
+                if(prev <= 1) {
+                    clearInterval(intervalRef.current);
+                    intervalRef.current = null;
+                    setStart(false);
+                    return 0;
+                }
+                return prev-1;
+            })
         },1000)
 
     }
@@ -37,11 +40,16 @@ function PomodoroTimer() {
             <div className="min-h-dvh bg-amber-100 flex flex-col items-center justify-around">
                 <h1 className="text-4xl text-amber-600">Pomodoro Timer</h1>
                 {!start && <Image src="/strawberry.png" alt="Image of a red strawberry" width={200} height={200}/>}
-                {start && <div className="text-6xl font-bold text-amber-600">{formatTime(time)}</div>}
-                <div className="flex flex-row gap-2">
-                <button className="btn btn-xl" onClick={startTimer}>Start</button>
-                <button className="btn btn-xl">End</button>
-                </div>
+                {start && <div className="text-6xl font-bold text-[#ea5951]">{formatTime(time)}</div>}
+
+                    {time === 0 && !rest &&
+                        <div className="flex flex-col gap-2">
+                        <div className="text-4xl font-bold text-[#ea5951]">Start Break?
+                        <button className="btn">Start Break</button>
+                        </div>
+                    </div>}
+
+
             </div>
         </>
     )
