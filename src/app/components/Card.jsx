c49ef2acdf84f3a69306d119747739e875f04fe4
vachan-maker <vachan@tutanota.com>
2025-08-01 08:26:20 +0530

    added the same hover effect to the border as well

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 export default function Card({service}) {
     return (
         <Link href={service.selfurl || service.exturl}>
-            <div className='border border-amber-200 rounded-md p-5 hover:bg-base-300 cursor-pointer bg-base-300 m-2 hover:shadow-2xl hover:scale-105 duration-500 ease-in-out'>
+            <div className='border border-transparent hover:border-[#fcce0c] rounded-md p-5 hover:bg-base-300 cursor-pointer bg-base-300 m-2 hover:shadow-2xl hover:scale-105 duration-500 ease-in-out'>
                 <p className='font-bold mb-1.5'>{service.name}</p>
                 <p>{service.description}</p>
                 {service.creator && <p className='mb-2 mt-4 text-sm'><b>Created by:</b> {service.creator}</p>}
