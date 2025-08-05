7684fff55f9074b09fc538aeeba8418b80b9113a
vachan-maker <vachan@tutanota.com>
2025-08-05 21:19:44 +0530

    reduce padding on mobile devices

--- src/app/components/ExploreColumn.jsx
+++ src/app/components/ExploreColumn.jsx
@@ -6,7 +6,7 @@ const RIT_Kutty = localFont({
 });
 export default function ExploreColumn({children,title}) {
     return (
-        <div className='p-10 flex flex-col gap-6'>
+        <div className='px-3 md:p-10 flex flex-col gap-6'>
             <div className='md:min-h-42 flex items-center justify-center'>
                 <h2 className={`${RIT_Kutty.className} text-center text-[#fcce0c] text-6xl p-5 md:text-6xl`}>{title}</h2>
             </div>
