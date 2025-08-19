a1dd35f3d093b7e8fbc29cfd767dca0c887144f4
vachan-maker <vachan@tutanota.com>
2025-08-19 20:15:56 +0530

    Added text for user to understand how long it takes to load the motivation

--- src/app/motivation/page.jsx
+++ src/app/motivation/page.jsx
@@ -9,6 +9,7 @@ function GenerateMotivation()
             <section className="mx-auto my-0 p-5 max-w-lg">
                 <h1 className="font-bold text-3xl text-center">Ignite</h1>
                 <p className="text-xl text-center">Fuel your fire.🔥</p>
+                <p className="text-xs text-center">Please wait a few minutes after pressing the button!</p>
                 <form action="/api/motivation" method="POST" className = "mt-5">
                 <div className="flex flex-col gap-5 items-center justify-center">
                     <textarea type="text" className="input w-full" name="motivation" required placeholder="What do you need motivation for?" rows={12}></textarea>
