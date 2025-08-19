d629cee7912db6bff5a0333b0ddd13b1b44f48cc
vachan-maker <vachan@tutanota.com>
2025-08-19 20:11:46 +0530

    Added adhils game

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card({service}) {
             <div className='border border-transparent hover:border-[#fcce0c] rounded-md p-5 hover:bg-base-300 cursor-pointer bg-base-300 m-2 hover:shadow-2xl hover:scale-105 duration-500 ease-in-out'>
                 <div className="flex flex-row gap-2">
                 <p className='font-bold mb-1.5'>{service.name}</p>
-                {service.new && <div className="badge badge-soft badge-info">new</div>}
+                {service.new && <div className="badge badge-soft badge-warning">new</div>}
                 </div>
                 <p>{service.description}</p>
                 {service.creator && <p className='mb-2 mt-4 text-sm'><b>Created by:</b> {service.creator}</p>}
--- src/app/motivation/page.jsx
+++ src/app/motivation/page.jsx
@@ -9,10 +9,10 @@ function GenerateMotivation()
             <section className="mx-auto my-0 p-5 max-w-lg">
                 <h1 className="font-bold text-3xl text-center">Ignite</h1>
                 <p className="text-xl text-center">Fuel your fire.🔥</p>
-                <form action="/api/motivation" method="POST">
+                <form action="/api/motivation" method="POST" className = "mt-5">
                 <div className="flex flex-col gap-5 items-center justify-center">
-                    <textarea type="text" className="input w-full" name="motivation" required placeholder="What do you need motivation for?" cols={6}></textarea>
-                    <button className='btn btn-accent'>Generate Random Text</button>
+                    <textarea type="text" className="input w-full" name="motivation" required placeholder="What do you need motivation for?" rows={12}></textarea>
+                    <button className='btn btn-accent'>Motivate me!</button>
                 </div>
                 </form>
             </section>
@@ -21,5 +21,4 @@ function GenerateMotivation()
 
 
 }
-
 export default GenerateMotivation
