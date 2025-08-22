7d60253fd0c1ea0d2eee5e087b66205e80e5c507
vachan-maker <vachan@tutanota.com>
2025-08-22 10:49:34 +0530

    Changed text in badge to ഫ്രഷ്

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
-export default function Card({service}) {
+export default function Card({ service }) {
     return (
         <Link href={service.selfurl || service.exturl}>
             <div className='border border-transparent hover:border-[#fcce0c] rounded-md p-5 hover:bg-base-300 cursor-pointer bg-base-300 m-2 hover:shadow-2xl hover:scale-105 duration-500 ease-in-out'>
                 <div className="flex flex-row gap-2">
-                <p className='font-bold mb-1.5'>{service.name}</p>
-                {service.new && <div className="badge badge-soft badge-warning">new</div>}
+                    <p className='font-bold mb-1.5'>{service.name}</p>
+                    {service.new && <div className="badge badge-soft badge-warning">ഫ്രഷ് </div>}
                 </div>
                 <p>{service.description}</p>
                 {service.creator && <p className='mb-2 mt-4 text-sm'><b>Created by:</b> {service.creator}</p>}
